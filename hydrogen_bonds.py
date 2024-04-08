bfa10a6960ea2b3a81e494d8353f52925ae87c57
pampelin <112780024+pampelin@users.noreply.github.com>
2024-04-08 09:48:35 +0200

    Add files via upload
    new version - with average bonds lengths

--- hydrogen_bonds.py
+++ hydrogen_bonds.py
@@ -23,7 +23,59 @@ for ii, name in enumerate(glob(f"/home/ebart/Dokumenty/preopt-atom-H-soubory/xtb
             bonds[bond_key].append(math.dist(conf.GetAtomPosition(a1_i), conf.GetAtomPosition(a2_i)))
 json.dump(bonds, open("bonds_raw.json", 'w'))
 
+###Zde vypisuju co mám v JSON
+#with open('bonds_raw.json', 'r') as file:
+#    data = json.load(file)
+#    numpy_array = np.array(data)
+#print(numpy_array)
+###Konec vypisování co mám v JSON
+
+
+#file_path = 'bonds_raw.json'
+#df = pd.read_json(file_path)
+
+#print(df.head())
+
+#x_column = 'x_column_name'
+#y_column = 'y_column_name'
+
+#plt.figure(figsize=(10, 6))
+#plt.plot(df[x_column], df[y_column], marker='o')
+#plt.title('Graf - vazby')
+#plt.xlabel(x_column)
+#plt.ylabel(y_column)
+#plt.show()
+
+
+#with open('bonds_raw.json', 'r') as file:
+#    data = json.load(file)
+#    numpy_array = np.array(data)
+#print(numpy_array)
+
+#dictionary = json.load(open('bonds_raw.json', 'r'))
+#xAxis = [key for key, value in dictionary.items()]
+#yAxis = [value for key, value in dictionary.items()]
+#plt.grid(True)
+#
+### LINE GRAPH ##
+#plt.plot(xAxis,yAxis, color='maroon', marker='o')
+#plt.xlabel('variable')
+#plt.ylabel('value')
+#
+#plt.show()
+
 with open('bonds_raw.json', 'r') as file:
-    data = json.load(file)
-    numpy_array = np.array(data)
-print(numpy_array)
+    bonds_data = json.load(file)
+
+# Výpočet průměrných délek vazeb
+average_bonds_lengths = {}
+for bond_key, lengths in bonds_data.items():
+    average_length = np.mean(lengths)
+    average_bonds_lengths[bond_key] = average_length
+
+# Uložení průměrů do nového JSON souboru
+output_path = 'average_bonds_lengths.json'
+with open(output_path, 'w') as file:
+    json.dump(average_bonds_lengths, file)
+
+print(f'Průměrné délky vazeb byly uloženy do souboru: {output_path}')
